fe9736cd4610a8576ebaa308eaa7c7f3bbf5068f
juliaaaaaaaaaaaaaaaa22 <juliavieradesouza200414@gmail.com>
2026-08-06 10:24:41 -0300

    feat: add Lista component with initial fruit list and structure

---
+++ app/lista/page.tsx
@@ -0,0 +1,16 @@
+export default function Lista() {
+    let frutas = ['maracuja', 'laranja', 'morango', 'uva', 'kiwi']
+
+    return (
+        <>
+        <h1>Lista</h1>
+        <ol type ="I">
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+            <li>item</li>
+         </ol>
+        </>
+    );
+}
