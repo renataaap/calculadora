1d9adc846d0634abe5b131775aa15e854f598495
juliaaaaaaaaaaaaaaaa22 <juliavieradesouza200414@gmail.com>
2026-08-06 10:34:37 -0300

    feat: add student list display to Lista component

--- app/lista/page.tsx
+++ app/lista/page.tsx
@@ -1,16 +1,46 @@
 export default function Lista() {
     let frutas = ['maracuja', 'laranja', 'morango', 'uva', 'kiwi']
 
+    let alunos = [
+        {
+            nome:"guilherme",
+            turma:"1ª A"
+        },
+        {
+            nome:"maria",
+            turma:"2º A"
+        },
+        {
+            nome:"joão",
+            turma:"3º A"
+        }
+    ]
+    
     return (
         <>
         <h1>Lista</h1>
         <ol type ="I">
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
-            <li>item</li>
+            <li>{frutas[0]}</li>
+            <li>{frutas[1]}</li>
+            <li>{frutas[2]}</li>
+            <li>{frutas[3]}</li>
+            <li>{frutas[4]}</li>
          </ol>
+        <h1>Lista de alunos</h1>
+        <ol type ="I">
+            <li> 
+                <p>Aluno:{alunos[0].nome} </p>
+                <p>Turma:{alunos[0].turma} </p>
+            </li>
+            <li> 
+                <p>Aluno:{alunos[1].nome} </p>
+                <p>Turma:{alunos[1].turma} </p>
+            </li>
+            <li> 
+                <p>Aluno:{alunos[2].nome} </p>
+                <p>Turma:{alunos[2].turma} </p>
+            </li>
+        </ol>
         </>
     );
 }
