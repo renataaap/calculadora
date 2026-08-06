713af9530f93ae0cbbc361bbec980bfa1d9be7e1
juliaaaaaaaaaaaaaaaa22 <juliavieradesouza200414@gmail.com>
2026-08-06 10:16:25 -0300

    refactor: improve structure and styling of the Materiais component

--- app/materiais/page.tsx
+++ app/materiais/page.tsx
@@ -1,23 +1,25 @@
 'use client'
-import { use, useState } from "react"
+import { useState } from "react"
 
 export default function Materiais() {
     const [ferramentas, setFerramentas] = useState('');
     return (
-        <>
+        <div className="informacoes">
             <h1>Materiais de construção</h1>
-            <button onClick={() => setFerramentas('1')}>Carriola</button>
-            <button onClick={() => setFerramentas('2')}>Pá de bico</button>
-            <button onClick={() => setFerramentas('3')}>Desempenadeira</button>
-            {ferramentas ==  '1' &&
+            <div className="botoes-info">
+                <button className="botao" onClick={() => setFerramentas('1')}>Carriola</button>
+                <button className="botao" onClick={() => setFerramentas('2')}>Pá de bico</button>
+                <button className="botao" onClick={() => setFerramentas('3')}>Desempenadeira</button>
+            </div>
+            {ferramentas == '1' &&
                 <p>Função: transportar cargas pesadas, entulhos e materiais de construção em geral.</p>
             }
-            {ferramentas ==  '2' &&
+            {ferramentas == '2' &&
                 <p>Função: cavar o solo e juntar ou carregar materiais soltos.</p>
             }
-            {ferramentas ==  '3' &&
+            {ferramentas == '3' &&
                 <p>Função: aplicar, nivelar e dar acabamento em argamassa, reboco ou gesso.</p>
             }
-        </>
+        </div>
     )
 }
